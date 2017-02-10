13afccfc55b2d3a612c13beeeebaaa1d8ff15bcb
shaozeming <szm19920426@gmail.com>
2017-02-10 17:36:49 +0800

    add failes

---
+++ src/wrapper/Search.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 4d4k
+ * Date: 2017/2/10
+ * Time: 17:30
+ */
+
+namespace shaozeming\xunsearch;
+
+
+class Search
+{
+    public static function info()
+    {
+        echo "这是自定义的Search  clas<br />";
+    }
+
+}
